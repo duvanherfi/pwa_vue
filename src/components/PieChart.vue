6b29e914e0a6c52dd37fa92e44dd00d69b7e4d35
Nora Rocio <nora.rocio23@gmail.com>
2024-04-04 17:03:26 -0500

    PWAB-31 Load data for chart dynamically

---
+++ src/components/PieChart.vue
@@ -0,0 +1,42 @@
+<template>
+  <div style="max-width: 500px">
+    <canvas ref="myChart"></canvas>
+  </div>
+</template>
+
+<script>
+import Chart from "chart.js/auto";
+
+export default {
+  name: "PieChart",
+  props: ["label", "chartData"],
+  methods: {
+    async loadChart() {
+      await new Chart(this.$refs.myChart.getContext("2d"), {
+        type: "pie",
+        data: {
+          labels: this.label,
+          datasets: [
+            {
+              label: "Cantidades",
+              data: this.chartData,
+            },
+          ],
+        },
+        options: {
+          scales: {
+            y: {
+              beginAtZero: true,
+            },
+          },
+        },
+      });
+    },
+  },
+  mounted() {
+    console.log(this.label);
+    console.log(this.chartData);
+    this.loadChart();
+  },
+};
+</script>
